fd5b274fa76303675e7128b31c81f59ea76723a2
djazayeri <djazayeri@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2009-06-16 01:55:44 +0000

    mdrtb module: added a relatively hacky javascript redirect that can be enabled to redirect certain roles from the home page to the mdrtb homepage

--- src/org/openmrs/module/mdrtb/MdrtbConstants.java
+++ src/org/openmrs/module/mdrtb/MdrtbConstants.java
@@ -3,5 +3,6 @@ package org.openmrs.module.mdrtb;
 public class MdrtbConstants {
     private static final String moduleName = "mdrtb";
     public static final String MDRTB_PROGRAM_GLOBAL_PROPERTY = moduleName + ".program_name";
+    public static final String ROLES_TO_REDIRECT_GLOBAL_PROPERTY = moduleName + ".roles_to_redirect_from_openmrs_homepage";
     public static final String MDRTB_PATIENT_IDENTIFIER_TYPES = moduleName + ".patient_identifier_type_list";
 }
---
+++ src/org/openmrs/module/mdrtb/extension/html/RedirectSomeRolesFromOpenmrsHomepage.java
@@ -0,0 +1,56 @@
+package org.openmrs.module.mdrtb.extension.html;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.openmrs.Role;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.Extension;
+import org.openmrs.module.mdrtb.MdrtbConstants;
+import org.openmrs.util.OpenmrsConstants;
+import org.springframework.util.StringUtils;
+
+public class RedirectSomeRolesFromOpenmrsHomepage extends Extension {
+
+    /**
+     * @see org.openmrs.module.Extension#getMediaType()
+     */
+    @Override
+    public MEDIA_TYPE getMediaType() {
+        return Extension.MEDIA_TYPE.html;
+    }
+
+    /**
+     * @see org.openmrs.module.Extension#getOverrideContent(java.lang.String)
+     * 
+     * @should do nothing if no user is logged in
+     * @should do nothing if the ROLES_TO_REDIRECT global property is not set
+     * @should do nothing if none of the authenticated users roles are in the ROLES_TO_REDIRECT global property
+     * @should redirect the user via javascript if one of the authenticated users roles is in the ROLES_TO_REDIRECT global property 
+     */
+    @Override
+    public String getOverrideContent(String str){
+        if (!Context.isAuthenticated()) {
+            return "";
+        }
+        Context.addProxyPrivilege(OpenmrsConstants.PRIV_VIEW_GLOBAL_PROPERTIES);
+        try {
+            String roleNamesToRedirect = Context.getAdministrationService().getGlobalProperty(MdrtbConstants.ROLES_TO_REDIRECT_GLOBAL_PROPERTY);
+            if (!StringUtils.hasText(roleNamesToRedirect)) {
+                return "";
+            }
+            String[] temp = roleNamesToRedirect.split(",");
+            Set<String> set = new HashSet<String>(Arrays.asList(temp));
+            for (Role role : Context.getAuthenticatedUser().getAllRoles()) {
+                if (set.contains(role.getRole())) {
+                    return "<script type=\"text/javascript\"> window.location = 'module/mdrtb/mdrtbIndex.form'; </script>";
+                }
+            }
+        } finally {
+            Context.removeProxyPrivilege(OpenmrsConstants.PRIV_VIEW_GLOBAL_PROPERTIES);
+        }
+        return "";        
+    }
+
+}
