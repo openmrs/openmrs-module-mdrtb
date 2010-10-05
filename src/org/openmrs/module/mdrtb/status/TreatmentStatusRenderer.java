66fcd861b9a05afe6fe8b663406ac5586315ff3e
mogoodrich <mogoodrich@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2010-10-05 14:30:10 +0000

    fixed file that was out-of-date, prob due to svn issues frmo last week

--- src/org/openmrs/module/mdrtb/status/TreatmentStatusRenderer.java
+++ src/org/openmrs/module/mdrtb/status/TreatmentStatusRenderer.java
@@ -1,10 +1,13 @@
 package org.openmrs.module.mdrtb.status;
 
+import org.openmrs.module.mdrtb.MdrtbConstants.TreatmentState;
 import org.openmrs.module.mdrtb.regimen.Regimen;
 
 
 public interface TreatmentStatusRenderer {
 
 	public String renderRegimen(Regimen regimen);
+
+	public String renderTreatmentState(TreatmentState state);
 
 }
