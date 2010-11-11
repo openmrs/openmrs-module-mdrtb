8a434e34ec030476c544ee8def9dab4aa8a7df07
mogoodrich <mogoodrich@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2010-11-11 17:42:20 +0000

    Bug fix to avoid null pointer exception; removed test class I checked in by accident

--- src/org/openmrs/module/mdrtb/comparator/PatientStateComparator.java
+++ src/org/openmrs/module/mdrtb/comparator/PatientStateComparator.java
@@ -8,8 +8,14 @@ import org.openmrs.PatientState;
 public class PatientStateComparator implements Comparator<PatientState> {
 
     public int compare(PatientState state1, PatientState state2) {
-    	// I don't worry about null cases here, because the start date for a patient state should never be null
-	    return state1.getStartDate().compareTo(state2.getStartDate());
+    	if (state1 == null || state1.getStartDate() == null) {
+    		return -1;
+    	}
+    	else if (state2 == null || state2.getStartDate() == null) {
+    		return 1;
+    	}
+    	else {
+    		return state1.getStartDate().compareTo(state2.getStartDate());
+    	}
     }
-
 }
